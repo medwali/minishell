041e30f6f7e882b8f09da5badd225842ee222f5b
Mohamed El idrissi el wali <mel-idri@e2r3p10.1337.ma>
2021-01-27 19:54:16 +0100

    fix SIGINT handling

--- create_child_process.c
+++ create_child_process.c
@@ -6,7 +6,7 @@
 /*   By: mel-idri <mel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/29 23:51:26 by mel-idri          #+#    #+#             */
-/*   Updated: 2021/01/27 18:45:11 by mel-idri         ###   ########.fr       */
+/*   Updated: 2021/01/27 19:51:14 by mel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,9 +16,10 @@ void	create_child_process(char *exe_path, char **argv, char **envp)
 {
 	pid_t	pid;
 
-	signal(SIGINT, SIG_DFL);
+	signal(SIGINT, SIG_IGN);
 	if ((pid = fork()) == 0)
 	{
+		signal(SIGINT, SIG_DFL);
 		if (execve(exe_path, argv, envp) == -1)
 			print_error("minishell: execve", NULL, E_EXECVE_FAILED);
 		exit(1);
