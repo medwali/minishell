d44c3adc02708cdf2d323abf98f1e7a639a441a4
Mohamed El idrissi el wali <mel-idri@e2r3p10.1337.ma>
2021-01-28 10:41:48 +0100

    fix norm

--- create_child_process.c
+++ create_child_process.c
@@ -6,19 +6,19 @@
 /*   By: mel-idri <mel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/29 23:51:26 by mel-idri          #+#    #+#             */
-/*   Updated: 2021/01/28 09:26:17 by mel-idri         ###   ########.fr       */
+/*   Updated: 2021/01/28 10:41:31 by mel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void print_newline(int signum)
+static void	print_newline(int signum)
 {
 	(void)signum;
 	ft_putchar('\n');
 }
 
-void	create_child_process(char *exe_path, char **argv, char **envp)
+void		create_child_process(char *exe_path, char **argv, char **envp)
 {
 	pid_t	pid;
 
