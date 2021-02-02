5579787105364c59b163271da5ddd0f0680074a5
Mohamed El idrissi el wali <mel-idri@e2r3p10.1337.ma>
2021-02-02 09:10:39 +0100

    fix leak in built-in pwd

--- builtin_pwd.c
+++ builtin_pwd.c
@@ -6,7 +6,7 @@
 /*   By: mel-idri <mel-idri@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/28 09:55:51 by mel-idri          #+#    #+#             */
-/*   Updated: 2021/01/28 10:10:23 by mel-idri         ###   ########.fr       */
+/*   Updated: 2021/02/02 09:02:16 by mel-idri         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,5 @@ void	builtin_pwd(void)
 		ft_putendl(cwd);
 	else
 		print_error("minshell: pwd:", NULL, E_CANT_GETCWD);
+	free(cwd);
 }
